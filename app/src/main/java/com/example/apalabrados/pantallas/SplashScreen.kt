080ec374aab0728f6a1b95a171bfc527df7f8ea1
Marcos <marcosmontanesdiad@gmail.com>
2023-02-08 10:53:11 +0100

    Splash Screen
    HE con justino terminado la splash screen

--- app/src/main/java/com/example/apalabrados/pantallas/SplashScreen.kt
+++ app/src/main/java/com/example/apalabrados/pantallas/SplashScreen.kt
@@ -1,24 +1,27 @@
 package com.example.apalabrados.pantallas
 
+import android.widget.ImageView
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.apalabrados.navegacion.PantallasApp
 import com.example.apalabrados.viewModel.ViewModel
 import kotlinx.coroutines.delay
-import java.lang.reflect.Modifier
+import com.example.apalabrados.R
+import com.example.apalabrados.ui.theme.AzulFondo
 
 @Composable
 fun SplashScreen(navController: NavController, ViewModel: ViewModel){
     LaunchedEffect(key1 = true){
-        delay(5000)
+        delay(3000)
         navController.popBackStack()
         navController.navigate(PantallasApp.Inicio.route)
     }
@@ -28,12 +31,14 @@ fun SplashScreen(navController: NavController, ViewModel: ViewModel){
 
 @Composable
 fun Splash(){
-    Column(
-        modifier = androidx.compose.ui.Modifier.fillMaxSize(),
+    Column (
+
+        modifier = androidx.compose.ui.Modifier.fillMaxSize().background(color = AzulFondo),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-    ) {
-        //Image(painter = , contentDescription = )
-        Text(text = "Bienvenidos a la app preguntaos")
+            ){
+
+        Image(painter = painterResource(id =R.drawable.preguntadoss ) , contentDescription = null, modifier = Modifier.size(400.dp))
     }
 }
+
