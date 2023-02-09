b03bff541dac57b08f2814a58b5b411644cd69f5
hgc88 <csanchezn3@alumnos.nebrija.es>
2023-02-09 16:32:16 +0100

    correcion viewmodel

--- app/src/main/java/com/example/apalabrados/viewModel/ViewModel.kt
+++ app/src/main/java/com/example/apalabrados/viewModel/ViewModel.kt
@@ -3,7 +3,6 @@ package com.example.apalabrados.viewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.apalabrados.model.MenuOption
 
 
 open class ViewModel : ViewModel() {
