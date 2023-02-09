43e2533f714ef9c22dc4af897567f6f97e4e9281
hgcsasdas <csanchezn3@alumnos.nebrija.es>
2023-02-09 14:14:28 +0100

    añadir preguntas
    falta que se guarden las respuestas

---
+++ app/src/main/java/com/example/apalabrados/model/MenuOption.kt
@@ -0,0 +1,3 @@
+package com.example.apalabrados.model
+
+data class MenuOption(val text: String, val value: Int)
--- app/src/main/java/com/example/apalabrados/viewModel/ViewModel.kt
+++ app/src/main/java/com/example/apalabrados/viewModel/ViewModel.kt
@@ -3,11 +3,10 @@ package com.example.apalabrados.viewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.apalabrados.model.MenuOption
+
 
 open class ViewModel : ViewModel() {
-
-    private val _campo = MutableLiveData<String>()
-    val campo: LiveData<String> = _campo
 
     private val _pregunta = MutableLiveData<String>()
     val pregunta: LiveData<String> = _pregunta
@@ -21,30 +20,43 @@ open class ViewModel : ViewModel() {
     private val _respuesta3 = MutableLiveData<String>()
     val respuesta3: LiveData<String> = _respuesta3
 
-    private val _correcta = MutableLiveData<String>()
-    val correcta: LiveData<String> = _correcta
+
+
+    private val _campo = MutableLiveData<String>()
+    val campo: LiveData<String> = _campo
+
+    private val _respuesta = MutableLiveData<String>()
+    val respuesta: LiveData<String> = _respuesta
+
     fun onCompletedFields(
-        campo: String,
         pregunta: String,
         respuesta1: String,
         respuesta2: String,
         respuesta3: String,
-        correcta: String
     ) {
-        _campo.value = campo
         _pregunta.value = pregunta
         _respuesta1.value = respuesta1
         _respuesta2.value = respuesta2
         _respuesta3.value = respuesta3
-        _correcta.value = correcta
+    }
+    fun campoSelected(
+        campo: String
+    ) {
+        _campo.value = campo
     }
 
+    fun respuestaSelected(
+        respuesta: String
+    ) {
+        _respuesta.value = respuesta
+    }
     fun limpiarCampos() {
         _campo.value = ""
         _pregunta.value = ""
         _respuesta1.value = ""
         _respuesta2.value = ""
         _respuesta3.value = ""
-        _correcta.value = ""
+        _respuesta.value = ""
     }
+
 }
